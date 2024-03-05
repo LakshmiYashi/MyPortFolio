0f78b7eea45cb998b7aacd6b5c379025383b8d80
laxcy <lakshmishreess@gmail.com>
2024-03-05 11:56:21 -0600

    ss diagnostic website

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,31 +2,39 @@ import Crypto from '../data/CryptoAdventurer.png';
 import Expense from '../data/ExpenseTracker.png';
 import TicTacToeLogo from '../data/TicTacToe.png';
 import WatchListLogo from '../data/MovieWatchList.png';
+import SSDiagnostics from '../data/SSDiagnostics.png'
 
 const projects = [
     {
         id: 1,
+        text:"SS Diagnostics",
+        img:SSDiagnostics,
+        description:"SS Diagnostics India website for precision healthcare solutions.",
+        githubLink:"https://ssdiagnosticsindia.com/"
+    },
+    {
+        id: 2,
         text:"Crypto Adventurer",
         img:Crypto,
         description:"Gives Detailed Insights On The Cryptocurrency Of Your Choice.",
         githubLink:"https://github.com/LakshmiYashi/CryptoAdventurer"
     },
     {
-        id: 2,
+        id: 3,
         text:"Expense Tracker",
         img:Expense,
         description:"Simplify budgeting with this user-friendly Expense Tracker.",
         githubLink:"https://github.com/LakshmiYashi/ExpensesTracker"
     },
     {
-        id: 3,
+        id: 4,
         text:" Authenticated Tic-Tac-Toe",
         img:TicTacToeLogo,
         description:"Enhance Tic-Tac-Toe with authentication for a personalized gaming twist.",
         githubLink:"https://github.com/LakshmiYashi/Auth_TicTacToe"
     },
     {
-        id: 4,
+        id: 5,
         text:"Movie WatchList",
         img:WatchListLogo,
         description:"Simplify movie choices with an easy-to-use watchlist/watched list feature.",
