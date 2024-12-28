d33e2714aecfddd1a924949ea78f0975f31c4b19
laxcy <lakshmishreess@gmail.com>
2024-12-27 22:12:52 -0600

    updating resume

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,12 +3,6 @@ import { Link } from 'react-router-dom';
 
 function Contact(){
     return (
-    //     <div className="contact-circle">
-    //     <p className="text-muted footer-title">Contact me!</p>
-    //     <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, nibh eu accumsan maximus, magna ligula varius lacus, sagittis imperdiet turpis erat facilisis mi. Mauris efficitur ac massa at malesuada.</p>
-    //     <br></br>
-    //     <Link to = {process.env.PUBLIC_URL + '/resume/lakshmi.docx'} target="_blank" download className="btn">DOWNLOAD_RESUME</Link>
-    //  </div>
     <div className="contact-card">
       <h2 className="text-muted footer-title">Let's Connect & Innovate</h2>
       <div className="contact-info text-muted">
